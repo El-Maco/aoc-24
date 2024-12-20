277ad5a763ed10aa2d2838ae465f06229ab33d56
El Maco <marcus.myllyviita@gmail.com>
2024-12-20 17:50:52 +0200

    Task 01 completed

--- src/challenges/day10.rs
+++ src/challenges/day10.rs
@@ -1,3 +1,8 @@
+extern crate queues;
+use std::collections::HashSet;
+
+use queues::*;
+use rayon::collections::hash_set;
 
 use crate::utils::{load_input, parse_input};
 
@@ -11,42 +16,132 @@ pub fn run() {
 
 type Trail = Vec<MapPosition>;
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 struct MapPosition {
     x: i32,
     y: i32,
-    height: Option<u8>,
+    height: Option<i32>,
 }
 
-fn find_trails(x: usize, y: usize, rows: &Vec<Vec<u8>>, trails: &mut Vec<Trail>) {
-    let height = rows[x][y];
-    let position: MapPosition = MapPosition {x: x as i32, y: y as i32, height: Some()};
-
-    let Some(new_pos) =
+impl MapPosition {
+    fn new(x: i32, y: i32, height: Option<i32>) -> Self {
+        MapPosition { x, y, height }
+    }
 }
 
-fn solve_part1(input: &str) -> u32 {
+fn get_valid_neighbors(position: &MapPosition, rows: &Vec<Vec<MapPosition>>) -> Vec<MapPosition> {
+    let mut valid_neighbors: Vec<MapPosition> = vec![];
+    // Right
+    if let Some(row) = rows.get(position.y as usize) {
+        if let Some(neighbor) = row.get((position.x + 1) as usize) {
+            if Some(neighbor.height) > Some(position.height) {
+                valid_neighbors.push(neighbor.clone());
+            }
+        }
+    }
+
+    // Down
+    if let Some(row) = rows.get((position.y + 1) as usize) {
+        if let Some(neighbor) = row.get(position.x as usize) {
+            if Some(neighbor.height) > Some(position.height) {
+                valid_neighbors.push(neighbor.clone());
+            }
+        }
+    }
+
+    // up
+    if position.y - 1 >= 0 {
+        if let Some(row) = rows.get((position.y - 1) as usize) {
+            if let Some(neighbor) = row.get(position.x as usize) {
+                if Some(neighbor.height) > Some(position.height) {
+                    valid_neighbors.push(neighbor.clone());
+                }
+            }
+        }
+    }
+
+    // Left
+    if position.x - 1 >= 0 {
+        if let Some(row) = rows.get(position.y as usize) {
+            if let Some(neighbor) = row.get((position.x - 1) as usize) {
+                if Some(neighbor.height) > Some(position.height) {
+                    valid_neighbors.push(neighbor.clone());
+                }
+            }
+        }
+    }
+    valid_neighbors
+}
+
+fn find_trails(x: i32, y: i32, rows: &Vec<Vec<MapPosition>>) -> HashSet<MapPosition> {
+    let trailhead = rows[y as usize][x as usize]; // Should be a 0
+    let mut trails: Queue<Trail> = queue![];
+    let root: Trail = vec![trailhead];
+    trails.add(root).unwrap();
+    let mut found_trails: HashSet<MapPosition> = HashSet::new();
+
+    loop {
+        let trail: Trail = match trails.remove() {
+            Ok(trail) => trail,
+            Err(e) => {
+                println!("Nothing in queue {}", e);
+                break;
+            }
+        };
+        let position = trail.last().unwrap();
+        if position.height.unwrap() == 9 && trail.len() == 10 {
+            found_trails.insert(*position);
+            continue;
+        }
+        let valid_neighbors: Vec<MapPosition> = get_valid_neighbors(position, &rows);
+        if valid_neighbors.len() == 0 {
+            continue;
+        }
+        valid_neighbors.iter().for_each(|neighbor| {
+            let mut new_trail: Trail = trail.clone();
+            new_trail.push(*neighbor);
+            trails.add(new_trail).unwrap();
+        });
+    }
+    found_trails
+}
+
+fn solve_part1(input: &str) -> i32 {
     let row = parse_input(input);
-    let map: Vec<Vec<u8>> = row.iter().map(|row| row.chars().map(|ch| ch.to_digit(10).expect("Could not parse digit") as u8).collect()).collect();
+    let map: Vec<Vec<MapPosition>> = row
+        .iter()
+        .enumerate()
+        .map(|(y, row)| {
+            row.chars()
+                .enumerate()
+                .map(|(x, ch)| {
+                    MapPosition::new(
+                        x as i32,
+                        y as i32,
+                        Some(ch.to_digit(10).expect("Failed to parse digit") as i32),
+                    )
+                })
+                .collect()
+        })
+        .collect();
 
-    let mut trails: Vec<Trail> = vec![];
+    let mut total_score: i32 = 0;
     map.iter().enumerate().for_each(|(j, row)| {
         row.iter().enumerate().for_each(|(i, d)| {
-            if *d == 0 {
-                let mut found_trails: Vec<Trail> = vec![];
-                find_trails(i, j, &map, &mut found_trails);
-                if found_trails.len() > 0 {
-                    trails.append(&mut found_trails);
-                }
-
+            if d.height.unwrap() == 0 {
+                let trails = find_trails(i as i32, j as i32, &map);
+                let trailhead_score = trails.len() as i32;
+                println!("Trailhead {:?} has score {trailhead_score}", map[j][i]);
+                println!("{:?}", trails);
+                total_score += trailhead_score;
             }
         });
     });
 
-
-    0
+    total_score
 }
 
-fn solve_part2(input: &str) -> u32 {
+fn solve_part2(input: &str) -> i32 {
     todo![]
 }
 
@@ -72,5 +167,4 @@ mod tests {
     fn test_solve_part2() {
         assert_eq!(solve_part2(TEST_INPUT), todo!());
     }
-
 }
--- src/utils.rs
+++ src/utils.rs
@@ -23,40 +23,33 @@ pub struct Position {
 
 impl Position {
     pub fn new(x: i32, y: i32) -> Self {
-        Position {
-            x,
-            y
-        }
+        Position { x, y }
     }
 
     pub fn is_contained(&self, width: i32, height: i32) -> bool {
         self.x >= 0 && self.y >= 0 && self.x < width && self.y < height
     }
 
-    pub fn distance_to(&self, position: &Position) -> f64 {
-        let (a, b) = ((position.x - self.x) as f64, (position.y - self.y) as f64);
-        let distance = (a.powi(2) + b.powi(2)).sqrt();
-        distance
-    }
+    // pub fn distance_to(&self, position: &Position) -> f64 {
+    //     let (a, b) = ((position.x - self.x) as f64, (position.y - self.y) as f64);
+    //     let distance = (a.powi(2) + b.powi(2)).sqrt();
+    //     distance
+    // }
 
     pub fn add(&self, position: &Position) -> Position {
-        Position::new(
-            self.x + position.x,
-            self.y + position.y
-        )
+        Position::new(self.x + position.x, self.y + position.y)
     }
 
-    pub fn exists_in(&self, positions: &Vec<Position>) -> bool {
-        positions.iter().any(|pos| self.eq(pos))
-    }
+    // pub fn exists_in(&self, positions: &Vec<Position>) -> bool {
+    //     positions.iter().any(|pos| self.eq(pos))
+    // }
 
-    pub fn eq(&self, position: &Position) -> bool {
-        self.x == position.x &&
-            self.y == position.y
-    }
+    // pub fn eq(&self, position: &Position) -> bool {
+    //     self.x == position.x &&
+    //         self.y == position.y
+    // }
 
     pub fn diff(&self, position: &Position) -> Position {
         Position::new(position.x - self.x, position.y - self.y)
     }
-
 }

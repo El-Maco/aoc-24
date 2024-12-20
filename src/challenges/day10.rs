4cf8be545fb2841e86d26ddbbc55c1bc9a0e99a8
El Maco <marcus.myllyviita@gmail.com>
2024-12-20 22:30:42 +0200

    Task 02 completed

--- src/challenges/day10.rs
+++ src/challenges/day10.rs
@@ -2,7 +2,6 @@ extern crate queues;
 use std::collections::HashSet;
 
 use queues::*;
-use rayon::collections::hash_set;
 
 use crate::utils::{load_input, parse_input};
 
@@ -84,13 +83,44 @@ fn find_trails(x: i32, y: i32, rows: &Vec<Vec<MapPosition>>) -> HashSet<MapPosit
         let trail: Trail = match trails.remove() {
             Ok(trail) => trail,
             Err(e) => {
-                println!("Nothing in queue {}", e);
                 break;
             }
         };
         let position = trail.last().unwrap();
         if position.height.unwrap() == 9 && trail.len() == 10 {
             found_trails.insert(*position);
+            continue;
+        }
+        let valid_neighbors: Vec<MapPosition> = get_valid_neighbors(position, &rows);
+        if valid_neighbors.len() == 0 {
+            continue;
+        }
+        valid_neighbors.iter().for_each(|neighbor| {
+            let mut new_trail: Trail = trail.clone();
+            new_trail.push(*neighbor);
+            trails.add(new_trail).unwrap();
+        });
+    }
+    found_trails
+}
+
+fn find_unique_trails(x: i32, y: i32, rows: &Vec<Vec<MapPosition>>) -> Vec<Trail> {
+    let trailhead = rows[y as usize][x as usize]; // Should be a 0
+    let mut trails: Queue<Trail> = queue![];
+    let root: Trail = vec![trailhead];
+    trails.add(root).unwrap();
+    let mut found_trails: Vec<Trail> = vec![];
+
+    loop {
+        let trail: Trail = match trails.remove() {
+            Ok(trail) => trail,
+            Err(e) => {
+                break;
+            }
+        };
+        let position = trail.last().unwrap();
+        if position.height.unwrap() == 9 && trail.len() == 10 {
+            found_trails.push(trail);
             continue;
         }
         let valid_neighbors: Vec<MapPosition> = get_valid_neighbors(position, &rows);
@@ -131,8 +161,6 @@ fn solve_part1(input: &str) -> i32 {
             if d.height.unwrap() == 0 {
                 let trails = find_trails(i as i32, j as i32, &map);
                 let trailhead_score = trails.len() as i32;
-                println!("Trailhead {:?} has score {trailhead_score}", map[j][i]);
-                println!("{:?}", trails);
                 total_score += trailhead_score;
             }
         });
@@ -142,7 +170,36 @@ fn solve_part1(input: &str) -> i32 {
 }
 
 fn solve_part2(input: &str) -> i32 {
-    todo![]
+    let row = parse_input(input);
+    let map: Vec<Vec<MapPosition>> = row
+        .iter()
+        .enumerate()
+        .map(|(y, row)| {
+            row.chars()
+                .enumerate()
+                .map(|(x, ch)| {
+                    MapPosition::new(
+                        x as i32,
+                        y as i32,
+                        Some(ch.to_digit(10).expect("Failed to parse digit") as i32),
+                    )
+                })
+                .collect()
+        })
+        .collect();
+
+    let mut total_score: i32 = 0;
+    map.iter().enumerate().for_each(|(j, row)| {
+        row.iter().enumerate().for_each(|(i, d)| {
+            if d.height.unwrap() == 0 {
+                let trails = find_unique_trails(i as i32, j as i32, &map);
+                let trailhead_score = trails.len() as i32;
+                total_score += trailhead_score;
+            }
+        });
+    });
+
+    total_score
 }
 
 #[cfg(test)]
@@ -165,6 +222,6 @@ mod tests {
 
     #[test]
     fn test_solve_part2() {
-        assert_eq!(solve_part2(TEST_INPUT), todo!());
+        assert_eq!(solve_part2(TEST_INPUT), 81);
     }
 }
